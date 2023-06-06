021197b7a1d841159c3ec130ce67d9423c06cb84
DESKTOP-OQQOBG6\owner <h0989541162@gmail.com>
2023-06-06 20:18:58 +0800

    去除外框空白處

--- src/view/catb.vue
+++ src/view/catb.vue
@@ -1,6 +1,5 @@
 <script setup>
 import catHeader from "../components/catHeader.vue";
-import { RouterLink, RouterView } from 'vue-router'
 import { onMounted, ref } from 'vue';
 
 /* header開關按紐 */
@@ -30,8 +29,8 @@ onMounted(() => {
 
 
 <template>
-  <div class="min-h-[100vh] w-full bg-#232526 bg-[url(src/assets/cat/Bg.png)] bg-no-repeat bg-[length:70%_50%] object-cover bg-center grid  grid-rows-[77px_minmax(calc(100vh-388px,1fr))_311px] grid-cols-[1fr_1fr_1fr]
-  lg:min-h-[100vh] lg:w-full lg:bg-[url(src/assets/cat/Bg.png)]  lg:bg-#232526 lg:bg-no-repeat lg:bg-[length:70%_100%] lg:object-cover lg:bg-right lg:grid lg:grid-rows-[105px_minmax(calc(100vh-309px,1fr))_204px] lg:grid-cols-[163px_1fr]
+  <div class="min-h-[100vh] w-full bg-#232526 bg-[url(src/assets/cat/Bg.png)] bg-no-repeat bg-contain bg-center grid  grid-rows-[77px_minmax(calc(100vh-388px,1fr))_311px] grid-cols-[1fr_1fr_1fr]
+  lg:min-h-[100vh] lg:bg-no-repeat lg:bg-auto lg:object-cover lg:bg-right lg:grid lg:grid-rows-[105px_minmax(calc(100vh-309px,1fr))_204px] lg:grid-cols-[163px_1fr]
   ">
     <!-- 左邊 -->
     <div class=" h100% row-start-1 row-end-2 col-start-1 col-end-4
@@ -40,7 +39,7 @@ onMounted(() => {
        lg:text-white lg:text-41px  lg:m-40px lg:font-bold lg:leading-none">CAT</p>
       <div class="absolute right-26px top-30px">
       <!-- 開起選單按鈕 -->
-        <img @click = isOpen src="../assets/cat/Vector.png" class="lg:hidden" alt="">
+        <img @click = isOpen src="../assets/cat/Vector.png" class="lg:hidden p-t-10px" alt="">
       </div>
       <div class=" hidden 
       lg:block lg:h-[calc(100vh-161px)] lg:flex lg:items-end lg:justify-center">
@@ -60,13 +59,61 @@ onMounted(() => {
     </div>
     <!-- w-0% h-100vh overflow-auto duration-500 fixed z-10 bg-#232526  -->
       <!-- Header -->
-      <catHeader></catHeader>
+      <header ref="header" class="w-full h-0% overflow-auto duration-500 fixed z-10 bg-#232526 
+        lg:block lg:row-start-1 lg:row-end-2 lg:col-start-2 lg:col-end-4 lg:w-auto lg:h-auto lg:static">
+        <nav class=" lg:w-90% lg:flex lg:flex-row-reverse lg:items-center lg:p-40px">
+          <ul class="flex flex-col items-center  text-white font-bold
+          lg:flex lg:flex-row lg:items-center" >
+            <!-- 關閉選單按鈕 -->
+            <li class="w-50px self-end lg:hidden">
+              <img @click = isOpen src="../assets/cat/close.svg"  class=" p-15px " alt="">
+            </li>
+              <li class="   lg:pr-55px  p-10px text-#EFC862  w-50px">
+                <router-link to="/cata" class="text-#EFC862">Home</router-link>
+              </li>
+              <li class="   lg:pr-55px  p-10px hover:text-#EFC862 w-50px">
+                <router-link to="/catb" class="text-#ffffff">Article</router-link>
+              </li>
+              <li class="   lg:pr-55px  p-10px hover:text-#EFC862 w-50px">
+                <router-link to="/catc" class="text-#ffffff">About</router-link>
+              </li>
+              <li class="   lg:pr-55px  p-10px hover:text-#EFC862 w-50px">
+                <router-link to="/about" class="text-#ffffff">Location</router-link>
+              </li> 
+              <li class="   lg:pr-55px  p-10px hover:text-#EFC862 w-50px">News</li>
+              <li class="p-20px w-50px lg:p-0 lg:w-fit">
+                <img src="../assets/cat/zoom_in_24px.png" alt="" class=" lg:w-full ">
+              </li>
+          </ul>
+        </nav>
+    </header>
       
 
       <!-- 主內容 -->
+      <main class="row-start-2 row-end-3 col-start-1 col-end-4 p-40px h-fit 
+        lg:flex lg:justify-center lg:row-start-2 lg:row-end-3 lg:col-start-2 lg:col-end-3 lg:p-20px lg:h-fit lg:mt-30px">
+          <div class="hidden
+            lg:block lg:w-1/2 lg:flex lg:flex-col lg:text-left lg:gap-4">
+            <p class=" lg:text-white lg:pb-10px lg:before:content-[''] lg:before:block lg:before:absolute  lg:before:bottom-0  lg:before:h-[1px]  lg:before:w-[50%]  lg:before:left-0  lg:before:right-0 lg:before:bg-[#A1A1A1]  lg:relative lg:after:content-[''] lg:after:block lg:after:absolute  after:bottom-0  lg:after:h-[1px]  lg:after:w-[25%]  lg:after:left-0  lg:after:right-0 lg:after:bg-[#EFC862]">/ 01</p>
+            <p class="lg:text-white">About Cat</p>
+            <p  class="lg:text-[#A1A1A1]">Senses</p>
+            <p  class="lg:text-[#A1A1A1]">Behavior</p>
+          </div>
+    
+          <div class="w-295px text-36px lg:w-[400px] lg:text-[72px]">
+            <p class=" w-295px h-100px leading-50px text-left pb-24px text-white font-bold
+            lg:leading-[72px] lg:text-left lg:pb-20px lg:text-white lg:w-auto lg:h-auto ">
+              EAT<br>
+              Everyday
+            </p>
+            <p class="text-11px text-left text-white leading-15px font-normal
+            lg:text-[21px] text-left lg:text-white leading-30px">
+              The cat is a domestic species of small carnivorous mammal.It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.
+            </p>
+          </div>
+      </main>
       
-      
-      <footer class="h-80% lg:flex lg:self-end row-start-3 row-end-4 col-start-1 col-end-4 backdrop-blur-[20px] bg-black/50 p-24px
+      <footer class="h-100% lg:flex lg:self-end row-start-3 row-end-4 col-start-1 col-end-4 backdrop-blur-[20px] bg-black/50 p-24px
         lg:h-100% lg:flex lg:self-end lg:row-start-3 lg:row-end-4 lg:col-start-2 lg:col-end-3 lg:backdrop-blur-[20px] lg:bg-black/50 lg:h-80% lg:p-0
       ">
         <div class="w-100% flex flex-col justify-center items-start  gap-2
@@ -89,7 +136,7 @@ onMounted(() => {
             <p class="text-[#EFC862]">Read more</p>
           </div>
         </div>
-    </footer>
+      </footer>
   </div>
  
 </template>
@@ -97,14 +144,14 @@ onMounted(() => {
 
 <style>
   *{
-    /* outline: 1px solid red; */
+    text-decoration: none;
   }
 
   
 
   @media screen and (max-width: 1024px) {
     #app{
-    padding: 0;
+    
   }
 }
 
