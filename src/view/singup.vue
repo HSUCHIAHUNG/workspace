7dc3af6d7c3ddf720e77b144810cf8f26d6e9d2d
DESKTOP-OQQOBG6\owner <h0989541162@gmail.com>
2023-06-14 13:08:45 +0800

    vee-validate 驗證表格製作

--- src/view/singup.vue
+++ src/view/singup.vue
@@ -17,16 +17,29 @@
 
         <h1 class="font-bold text-36px m-b-24px leading-[1] text-white lg:text-72px ">註冊</h1>
 
-        <V-Form @submit="onSubmit" class="flex flex-col gap-5px max-w-50% min-w-300px">
+        <V-Form @submit="onSubmit" class="flex flex-col gap-10px max-w-50% min-w-400px">
           
-          <label for="username">123</label>
-          <V-Field name="username" rules="required"></V-Field>
-          <V-ErrMsg name="username"></V-ErrMsg>
-          
-          <V-Field name="password" rules="required" type="password"></V-Field>
-          <V-ErrMsg name="password"></V-ErrMsg>
+          <label for="username" class=" flex flex-col gap-10px text-white p-t-5px"> 姓名
+            <V-Field name="username" rules="required" placeholder="請輸入姓名" class=" rounded-5 p-10px border-none "></V-Field>
+            <V-ErrMsg name="username" class="text-#EFC862 "></V-ErrMsg>
+          </label>
 
-          <button type="submit">登入</button>
+          <label for="userId" class=" flex flex-col gap-10px text-white p-t-5px"> 帳號
+            <V-Field name="userId" rules="required" placeholder="請輸入5-20碼英文加數字的帳號" class=" rounded-15px p-10px border-none"></V-Field>
+            <V-ErrMsg name="userId" class="text-#EFC862 "></V-ErrMsg>
+          </label>
+
+          <label for="password" class=" flex flex-col gap-10px text-white p-t-5px"> 密碼
+            <V-Field name="password" rules="required" type="password" placeholder="請輸入6-12碼英文加數字的密碼" class="rounded-15px p-10px border-none"></V-Field>
+            <V-ErrMsg name="password" class="text-#EFC862 "></V-ErrMsg>
+          </label>
+
+          <label for="password_check" class=" flex flex-col gap-10px text-white p-t-5px"> 密碼
+            <V-Field name="password_check" rules="required" type="password" placeholder="請再次輸入密碼" class="rounded-15px p-10px border-none"></V-Field>
+            <V-ErrMsg name="password_check" class="text-#EFC862 "></V-ErrMsg>
+          </label>
+          
+          <button class="m-t-15px rounded-15px p-10px border-none">登入</button>
 
         </V-Form>
 
@@ -36,6 +49,6 @@
 
 <style>
   *{
-    outline: 1px solid red;
+    /* outline: 1px solid red; */
   }
 </style>
